0b23467d33e06370bb7829dba259bfd3cd35134f
andreshalac <andreshalac@gmail.com>
2022-07-21 20:05:22 +0000

    empezamos a agregar models.py

--- src/api/models.py
+++ src/api/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,3 +17,13 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Trabajadores(db.Model): 
+     __tablename__ = 'trabajadores'
+     trabajador_id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(250), nullable=False)
+     email = db.Column(db.String(250), nullable=False)
+     address = db.Column(db.String(250))
+     phone = db.Column(db.String(250))
+     
+
