4bd039140050bc83d30342a016001f75370d74db
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-07-22 07:06:49 +0000

    empezando home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <h1 className="navbar-brand ms-5">Worky</h1>
+        <form className="d-flex">
+          <button className="btn btn-success me-3" type="button">
+            Iniciar Sesión
+          </button>
+          <button className="btn btn-outline-warning me-5" type="button">
+            Registrarse
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,22 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container d-flex mx-auto my-4">
+      <div className="imagen-trabajadores me-5">
+        <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
+          <h1 className="texto-imagen py-2">Trabajadores</h1>
+        </div>
+      </div>
+      <div className="imagen-empleadores ms-5">
+        <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
+          <h1 className="texto-imagen py-2">Contratantes</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
