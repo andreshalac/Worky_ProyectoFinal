0de073043369d7efc37e194923c347b206e006aa
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-07-26 18:06:57 +0000

    home casi terminado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/home.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar bg-light">
+    <nav className="navbar fixed-top">
       <div className="container-fluid">
         <h1 className="navbar-brand ms-5">Worky</h1>
-        <form className="d-flex">
-          <button className="btn btn-success me-3" type="button">
+        <div className="d-flex">
+          <button className="btn btn-primary me-3" type="button">
             Iniciar Sesión
           </button>
-          <button className="btn btn-outline-warning me-5" type="button">
+          <button className="btn btn-success me-5" type="button">
             Registrarse
           </button>
-        </form>
+        </div>
       </div>
     </nav>
   );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,17 +6,35 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container d-flex mx-auto my-4">
-      <div className="imagen-trabajadores me-5">
-        <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
-          <h1 className="texto-imagen py-2">Trabajadores</h1>
+    <>
+      <div className="container d-flex mx-auto my-4 mt-5 pt-4">
+        <div className="imagen-trabajadores me-5">
+          <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
+            <h1 className="texto-imagen py-2">Trabajadores</h1>
+          </div>
+        </div>
+        <div className="imagen-empleadores ms-5">
+          <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
+            <h1 className="texto-imagen py-2">Contratantes</h1>
+          </div>
         </div>
       </div>
-      <div className="imagen-empleadores ms-5">
-        <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
-          <h1 className="texto-imagen py-2">Contratantes</h1>
-        </div>
+      <div className="mx-5 my-5">
+        <h2>
+          <strong>¿Que es worky?</strong>
+        </h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
       </div>
-    </div>
+    </>
   );
 };
