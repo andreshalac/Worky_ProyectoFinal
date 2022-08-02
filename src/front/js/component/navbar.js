8c0997fe39fb60c3c5e583fa3ebe86d289d13f35
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-08-02 18:53:17 +0000

    cambios de estilo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,7 +8,10 @@ export const Navbar = () => {
       <div className="container-fluid">
         <h1 className="navbar-brand ms-5">Worky</h1>
         <div className="d-flex">
-          <button className="btn btn-primary me-3" type="button">
+          <button
+            className="boton-iniciar-sesion btn btn-info me-3"
+            type="button"
+          >
             Iniciar Sesión
           </button>
           <button className="btn btn-success me-5" type="button">
