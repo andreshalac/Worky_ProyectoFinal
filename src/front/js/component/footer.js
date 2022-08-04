11db2e797b91289cab05bfe3d376af5f081fd33f
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-08-04 18:35:54 +0000

    cambios estructura

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
 import "../../styles/home.css";
 
-export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center bg-dark text-white">
-    <p>Made by Andrés & Daniel.</p>
-  </footer>
-);
+export const Footer = () => <></>;
