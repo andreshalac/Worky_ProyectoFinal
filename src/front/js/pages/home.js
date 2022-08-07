ff56dfa6d4d47a2d4bc0672df20b5662076b3832
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-08-07 16:41:29 +0000

    borrador trabajadores

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -30,17 +30,13 @@ export const Home = () => {
       </nav>
       <div className="container d-flex mx-auto my-4 mt-5 pt-4">
         <div className="imagen-trabajadores me-5">
-          <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
-            <Link className="texto-imagen" to="">
-              <h1 className="py-2 text-dark">Trabajadores</h1>
-            </Link>
+          <div className="bg-white p-2 bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
+            <h1 className="py-2 text-dark">Trabajadores</h1>
           </div>
         </div>
         <div className="imagen-empleadores ms-5">
           <div className="bg-white p-2 text-dark bg-opacity-50 text-center position-relative top-50 start-50 translate-middle">
-            <Link className="texto-imagen" to="">
-              <h1 className="py-2 text-dark">Contratantes</h1>
-            </Link>
+            <h1 className="py-2">Contratantes</h1>
           </div>
         </div>
       </div>
---
+++ src/front/js/pages/trabajadores.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../../styles/trabajadores.css";
+import { Link } from "react-router-dom";
+
+export const Trabajadores = () => {
+  return (
+    <div>
+      <div className="container">
+        <div class="card">
+          <h5 class="card-header">Featured</h5>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
